dc151ce28ba626f2faff76b98a7fca399f4b6b0c
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-08 09:03:41 +0100

    index done

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+@section('title', 'projects')
+
 @section('content')
-<h1>palle</h1>
+<ul class="row d-flex justify-content-center">
+    @foreach ($projects as $project)
+    <li class="col-2 p-2 m-1 card">
+        <h4>{{$project->title}}</h4>
+        <img src="{{$project->image}}" class="my-2">
+        <div>
+            <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-small btn-success">Vedi</a>
+        </div>
+    </li>
+    @endforeach
+</ul>
 @endsection
